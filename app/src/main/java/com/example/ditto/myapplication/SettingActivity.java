0d5575fdea7f99e4b75403ae9ec23812147763e1
Ditto17 <ansh159@gmail.com>
2018-10-11 21:19:29 +0530

    Solved profile picture error | Profile Setting page is completed

--- app/src/main/java/com/example/ditto/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/ditto/myapplication/SettingActivity.java
@@ -57,22 +57,26 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        //Finding widget with their uniqe ID's
         mDisplayImage = (CircleImageView) findViewById(R.id.settings_image);
         mName = (TextView) findViewById(R.id.settings_display_name);
         mStatus = (TextView) findViewById(R.id.settings_status);
-
         mStatusbtn = (Button) findViewById(R.id.settings_status_btn);
-
         mImagebtn = (Button) findViewById(R.id.settings_image_btn);
 
+        //Getting the Instance of current User
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        //Getting the storage ref from the firebaseStorage
         mImageStorage = FirebaseStorage.getInstance().getReference();
 
+        //Getting the UID of the current user
         String current_uid = mCurrentUser.getUid();
 
+        //poplating the realtimedatabase with USER --> UID
         mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
 
+        //setting up an valueEventlistener
         mUserDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -85,9 +89,9 @@ public class SettingActivity extends AppCompatActivity {
                 mName.setText(name);
                 mStatus.setText(status);
 
-                if(!image.equals("default")){
+                //if(!image.equals("default")){
                     Picasso.get().load(image).into(mDisplayImage);
-                }
+
             }
 
             @Override
@@ -96,12 +100,15 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
+        //setting up an OnClicklistener on change status button
         mStatusbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //Getting the status from the realtime database and storing it in a string.
                 String status_value = mStatus.getText().toString();
 
+                //Making an Intent to Move the user to SettingsActivity --> StatusActivity.
                 Intent status_intent = new Intent(SettingActivity.this, StatusActivity.class);
                 status_intent.putExtra("status_value", status_value);
 
@@ -110,14 +117,21 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
+        //Setting up an OnClickListener on an ImageButton.
         mImagebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //Using an Intent to get all the images from the gallery.
                 Intent galleryIntent = new Intent();
+
+                //Setting up the path of the image source.
                 galleryIntent.setType("image/*");
+
+                //Getting the content(images) from the above path.
                 galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
 
+                //Returning the Activity with a result(selected Image).
                 startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
 
                 // start picker to get image for cropping and then use the image in cropping activity
@@ -140,11 +154,13 @@ public class SettingActivity extends AppCompatActivity {
             Uri imageUri = data.getData();
 
             // start cropping activity for pre-acquired image saved on the device
+            //Instantiating the cropImage feature and setting the ratio in 1:1.
             CropImage.activity(imageUri)
                     .setAspectRatio(1, 1)
-                    .start(this);
+                    .start(SettingActivity.this);
         }
 
+        //Checking if the image is cropped or not.
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
@@ -158,67 +174,60 @@ public class SettingActivity extends AppCompatActivity {
 
                 Uri resultUri = result.getUri();
 
-                String current_user_id = mCurrentUser.getUid();
+                final String current_user_id = mCurrentUser.getUid();
 
-                StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id + ".jpg");
-                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                final StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id + ".jpg");
 
-                        if(task.isSuccessful()) {
+                if(resultUri != null) {
+
+                    filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                            if (task.isSuccessful()) {
 
 
-                            //@SuppressWarnings("VisibleForTests") String download_url = task.getResult().getStorage().getDownloadUrl().toString();
-                            mImageStorage.child("profile_images").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
+                                //@SuppressWarnings("VisibleForTests") String download_url = task.getResult().getStorage().getDownloadUrl().toString();
+                                mImageStorage.child("profile_images").child(current_user_id + ".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
 
-                                    String download_url = uri.toString();
+                                        String download_url = uri.toString();
 
-                                    mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        //check again .child("image")
+                                        mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
 
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
 
-                             if(task.isSuccessful()){
+                                                if (task.isSuccessful()) {
 
-                                 mProgressDialog.dismiss();
-                                 Toast.makeText(SettingActivity.this, "uploaded.", Toast.LENGTH_LONG).show();
+                                                    mProgressDialog.dismiss();
+                                                    Toast.makeText(SettingActivity.this, "uploaded.", Toast.LENGTH_LONG).show();
 
-                             }
+                                                }
+
+                                            }
+                                        });
+
+                                    }
+                                });
+
+                            } else {
+
+                                Toast.makeText(SettingActivity.this, "Error in uploading.", Toast.LENGTH_LONG).show();
+                                mProgressDialog.dismiss();
 
                             }
-                        });
-
-                                }
-                            });
 
                         }
-                                else {
-
-                            Toast.makeText(SettingActivity.this, "Error in uploading.", Toast.LENGTH_LONG).show();
-                            mProgressDialog.dismiss();
-
-                        }
-
-                    }
-                });
+                    });
+                }
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
         }
     }
-   /* public static String random() {
-        Random generator = new Random();
-        StringBuilder randomStringBuilder = new StringBuilder();
-        int randomLength = generator.nextInt( 99);
-        char tempChar;
-        for (int i = 0; i < randomLength; i++){
-            tempChar = (char) (generator.nextInt(96) + 32);
-            randomStringBuilder.append(tempChar);
-        }
-        return randomStringBuilder.toString();
-    }*/
 
 }
